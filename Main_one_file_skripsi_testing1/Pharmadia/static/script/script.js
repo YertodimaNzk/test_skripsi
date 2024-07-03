cf3963e77f04daccbbbdd32caab9ca89c2edd9db
Yertodima <92780200+YertodimaNzk@users.noreply.github.com>
2024-07-03 14:12:18 +0700

    update voice on index.html

--- Main_one_file_skripsi_testing1/Pharmadia/static/script/script.js
+++ Main_one_file_skripsi_testing1/Pharmadia/static/script/script.js
@@ -2,27 +2,31 @@
 document.getElementById("diseaseInput").addEventListener("keypress", function(event) {
     if (event.key === "Enter") { 
         event.preventDefault();
-        var query = document.getElementById("diseaseInput").value.trim();
-        if (query !== '') {
-            // Melakukan redirection ke halaman result page
-            window.location.href = "/result-page?query=" + encodeURIComponent(query);
-        }
+        performSearch();
     }
 });
 
-// Inisialisasi recognition object untuk speech-to-text
-var recognition = new webkitSpeechRecognition() || new SpeechRecognition();
-recognition.lang = 'id-ID'; // Bahasa Indonesia, sesuaikan jika diperlukan
-
-// Event listener untuk hasil speech recognition
-recognition.onresult = function(event) {
-    var result = event.results[0][0].transcript;
-    document.getElementById('diseaseInput').value = result;
-    console.log('Speech Recognition:', result);
-    searchDisease(); // Memanggil fungsi pencarian dengan hasil speech recognition
+function performSearch() {
+    var query = document.getElementById("diseaseInput").value.trim();
+    if (query !== '') {
+        // Melakukan redirection ke halaman result page
+        window.location.href = "/result-page?query=" + encodeURIComponent(query);
+    }
 }
 
-// Fungsi untuk memulai speech recognition
 function startSpeechRecognition() {
+    // Inisialisasi recognition object untuk speech-to-text
+    var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition.lang = 'id-ID'; // Bahasa Indonesia, sesuaikan jika diperlukan
+
+    // Event listener untuk hasil speech recognition
+    recognition.onresult = function(event) {
+        var result = event.results[0][0].transcript;
+        document.getElementById('diseaseInput').value = result;
+        console.log('Speech Recognition:', result);
+        performSearch(); // Memanggil fungsi pencarian dengan hasil speech recognition
+    }
+
+    // Start recognition
     recognition.start();
 }
